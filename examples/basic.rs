02ea56f83e95612d814cdf46489c67701ac78774
Francis Tseng <f+accounts@frnsys.com>
2016-12-30 14:51:27 -0500

    working basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,22 +1,13 @@
 extern crate sim;
 extern crate rustc_serialize;
 
-use std::sync::{Arc, RwLock};
-use sim::{Agent, Manager, LocalManager, State, AgentProxy, AgentPath, SharedPopulation};
+use std::thread;
+use sim::{Agent, Manager, Simulation, Population, Worker, Uuid};
 
-// TODO may be possible to even do an enum of states? that's how you could represent different
-// agent types
 #[derive(RustcDecodable, RustcEncodable, Debug, PartialEq, Clone)]
 pub struct MyState {
     name: String,
     health: usize,
-}
-
-#[derive(Debug, Clone)]
-pub struct MyAgent {
-    id: usize,
-    state: MyState,
-    updates: Vec<MyUpdate>,
 }
 
 #[derive(RustcDecodable, RustcEncodable, Debug, PartialEq, Clone)]
@@ -30,71 +21,56 @@ pub enum MyUpdate {
     ChangeHealth(usize),
 }
 
-impl Agent for MyAgent {
+#[derive(Clone)]
+pub struct MySimulation;
+
+impl Simulation for MySimulation {
     type State = MyState;
     type Update = MyUpdate;
     type World = MyWorld;
-    fn new(state: MyState, id: usize) -> MyAgent {
-        MyAgent {
-            id: id,
-            state: state,
-            updates: Vec::new(),
-        }
-    }
-    fn id(&self) -> usize {
-        self.id
-    }
-    fn setup(&mut self, world: &Self::World) -> () {}
-    fn decide(&self,
-              world: Self::World,
-              population: SharedPopulation<Self>)
-              -> Vec<(AgentPath, Self::Update)> {
-        let mut updates = Vec::new();
-        match self.state.name.as_ref() {
-            "hello" => {
-                println!("my name is hello");
-                let pop = population.read().unwrap();
-                match pop.find(|s| s.name == "goodbye") {
-                    Some(a) => {
-                        println!("other name: {:?}", a);
-                        updates.push((a.path, MyUpdate::ChangeHealth(12)));
-                    }
-                    None => println!("not found"),
-                }
 
+    fn setup(&self, agent: Agent<Self::State>, population: &Population<Self>) -> () {
+        population.index(agent.state.name.as_ref(), &agent);
+    }
+
+    fn decide(&self,
+              agent: Agent<Self::State>,
+              world: Self::World,
+              population: &Population<Self>)
+              -> Vec<(Uuid, Self::Update)> {
+        let mut updates = Vec::new();
+        match agent.state.name.as_ref() {
+            "hello" => {
+                let agents = population.lookup("goodbye");
+                for a in agents {
+                    updates.push((a.id, MyUpdate::ChangeHealth(12)));
+                }
             }
-            "goodbye" => println!("my name is goodbye"),
+            "goodbye" => (),
             _ => println!("my name is unknown"),
         }
         updates
     }
-    fn state(&self) -> MyState {
-        self.state.clone()
-    }
-    fn set_state(&mut self, state: MyState) -> () {
-        self.state = state;
-    }
-    fn updates(&self) -> &Vec<MyUpdate> {
-        &self.updates
-    }
-    fn queue_updates(&mut self, updates: &mut Vec<MyUpdate>) -> () {
-        self.updates.append(updates);
-    }
-    fn apply_update(&self, state: MyState, update: MyUpdate) -> Self::State {
-        match update {
-            MyUpdate::ChangeName(name) => {
-                MyState {
-                    name: name,
-                    health: state.health,
+
+    fn update(&self, state: Self::State, updates: Vec<Self::Update>) -> Self::State {
+        let mut state = state.clone();
+        for update in updates {
+            state = match update {
+                MyUpdate::ChangeName(name) => {
+                    MyState {
+                        name: name,
+                        health: state.health,
+                    }
                 }
-            }
-            MyUpdate::ChangeHealth(health) => {
-                MyState {
-                    name: state.name,
-                    health: state.health + health,
+                MyUpdate::ChangeHealth(health) => {
+                    MyState {
+                        name: state.name,
+                        health: state.health + health,
+                    }
                 }
             }
         }
+        state
     }
 }
 
@@ -108,20 +84,37 @@ fn main() {
         name: "goodbye".to_string(),
         health: health,
     };
+    let addr = "redis://127.0.0.1/";
     let world = MyWorld { weather: "sunny".to_string() };
-    let mut manager = LocalManager::<MyAgent>::new(world);
-    {
-        let mut pop = manager.population.write().unwrap();
-        pop.spawn(state.clone());
-        pop.spawn(state2.clone());
-    }
-    manager.decide();
-    manager.update();
-    {
-        let pop = manager.population.read().unwrap();
-        let a = pop.get(AgentPath::Local(1)).unwrap();
-        println!("{:?}", a);
-        println!("ok");
-        assert_eq!(a.state.health, health + 12);
-    }
+    let sim = MySimulation {};
+    let mut manager = Manager::<MySimulation>::new(addr, sim, world);
+
+    manager.population.reset_indices();
+
+    manager.population.spawn(state.clone());
+    let id = manager.population.spawn(state2.clone());
+    assert_eq!(manager.population.count(), 2);
+
+    let worker_t = thread::spawn(move || {
+        let sim = MySimulation {};
+        let worker = Worker::new(addr, sim);
+        worker.start();
+    });
+
+    let n_steps = 10;
+    let manager_t = thread::spawn(move || {
+        manager.start(n_steps);
+        manager
+    });
+
+    manager = manager_t.join().unwrap();
+    worker_t.join().unwrap();
+
+    let agent = match manager.population.get(id) {
+        Some(a) => a,
+        None => panic!("Couldn't find the agent"),
+    };
+
+    println!("{:?}", agent);
+    assert_eq!(agent.state.health, health + (12 * n_steps));
 }
